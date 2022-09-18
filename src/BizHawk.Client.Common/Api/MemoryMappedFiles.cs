971e83bb53f2512839e874d8041cb3720448a944
Eddie Ringle <eddie@ringle.io>
2022-09-18 16:52:18 -0500

    wip: mmap Lua APIs
    Signed-off-by: Eddie Ringle <eddie@ringle.io>

--- src/BizHawk.Client.Common/Api/MemoryMappedFiles.cs
+++ src/BizHawk.Client.Common/Api/MemoryMappedFiles.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.MemoryMappedFiles;
 using System.Text;
 
@@ -8,6 +9,8 @@ namespace BizHawk.Client.Common
 	public sealed class MemoryMappedFiles
 	{
 		private readonly Dictionary<string, MemoryMappedFile> _mmfFiles = new Dictionary<string, MemoryMappedFile>();
+
+		private readonly Dictionary<int, MemoryMappedFile> _mmfFilesOnDisk = new Dictionary<int, MemoryMappedFile>();
 
 		private readonly Func<byte[]> _takeScreenshotCallback;
 
@@ -46,6 +49,74 @@ namespace BizHawk.Client.Common
 				return 0;
 			}
 			return WriteToFile(Filename, _takeScreenshotCallback());
+		}
+
+		public (MemoryMappedFile MappedFile, int Handle) OpenOrCreateFile(string filePath, long size)
+		{
+			var fileName = Path.GetFileName(filePath);
+			var file = File.Open(
+				path: filePath,
+				mode: FileMode.OpenOrCreate,
+				access: FileAccess.ReadWrite,
+				share: FileShare.ReadWrite
+			);
+			var mmf = MemoryMappedFile.CreateFromFile(
+				fileStream: file,
+				mapName: fileName,
+				capacity: size,
+				access: MemoryMappedFileAccess.ReadWrite,
+				inheritability: HandleInheritability.Inheritable,
+				leaveOpen: false
+			);
+			var handle = mmf.SafeMemoryMappedFileHandle.DangerousGetHandle()
+				.ToInt32();
+			_mmfFilesOnDisk[handle] = mmf;
+			return (MappedFile: mmf, Handle: handle);
+		}
+
+		public int WriteToMappedFile(int handle, byte[] outputBytes)
+		{
+			if (!_mmfFilesOnDisk.TryGetValue(handle, out var mmf))
+			{
+				throw new IOException($"No cached file found for given handle '{handle}'");
+			}
+
+			if (mmf.SafeMemoryMappedFileHandle.IsClosed)
+			{
+				throw new IOException($"Mapped file handle '{handle}' is closed");
+			}
+			
+			if (mmf.SafeMemoryMappedFileHandle.IsInvalid)
+			{
+				throw new IOException($"Mapped file handle '{handle}' is invalid");
+			}
+			
+			using var accessor = mmf.CreateViewAccessor(0, outputBytes.LongLength, MemoryMappedFileAccess.ReadWrite);
+			accessor.WriteArray(0, outputBytes, 0, outputBytes.Length);
+			return outputBytes.Length;
+		}
+
+		public bool DisposeMappedFile(int handle)
+		{
+			if (!_mmfFilesOnDisk.TryGetValue(handle, out var mmf))
+			{
+				return false;
+			}
+
+			_mmfFilesOnDisk.Remove(handle);
+
+			if (mmf.SafeMemoryMappedFileHandle.IsClosed)
+			{
+				return false;
+			}
+			
+			if (mmf.SafeMemoryMappedFileHandle.IsInvalid)
+			{
+				return false;
+			}
+			
+			mmf.Dispose();
+			return true;
 		}
 
 		public int WriteToFile(string filename, byte[] outputBytes)
